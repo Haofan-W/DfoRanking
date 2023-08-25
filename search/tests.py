d7cabf2ceea9d6b0b396a4e853cd5ddc30602127
Haofan Wang <90638309+N17270182@users.noreply.github.com>
2023-08-25 18:58:01 -0500

    add test for search vaild character

--- search/tests.py
+++ search/tests.py
@@ -1,3 +1,41 @@
 from django.test import TestCase
+from django.urls import reverse
+from unittest.mock import patch
+from decouple import config
 
-# Create your tests here.
+from .forms import CharacterSearchForm  # Make sure to import your form
+from .views import characterResultView
+
+
+class CharacterResultViewTest(TestCase):
+
+    @patch('search.views.requests.get')
+    def test_search_character(self, mock_get):
+        api_response = {
+            "rows": [
+                {
+                    "serverId": "cain",
+                    "characterId": "c54f3097ef542dca63d604d78f13d626",
+                    "characterName": "blindRaiden",
+                    "level": 110,
+                    "jobId": "40132cbc8b2b5eedfe035e35c322472e",
+                    "jobGrowId": "ec6a93f4d14bb36ccc541183291197a7",
+                    "jobName": "Slayer (M)",
+                    "jobGrowName": "Neo: Asura",
+                    "fame": 49408
+                }
+            ]
+        }
+        mock_get.return_value.status_code = 200
+        mock_get.return_value.json.return_value = api_response
+
+        url = reverse('search:character_result')
+        response = self.client.get(url, {'character_search_query': 'blindRaiden'})
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'search/characterResult.html')
+
+        character = response.context['character']['rows'][0]
+        self.assertEqual(character['characterName'], "blindRaiden")
+        self.assertEqual(character['jobName'], "Slayer (M)")
+        self.assertEqual(character['jobGrowName'], "Neo: Asura")
